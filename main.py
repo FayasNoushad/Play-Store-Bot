f75714e09099c50fd087e7b1309de957f4f723fc
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-18 19:02:19 +0530

    Added Bot variable

--- main.py
+++ main.py
@@ -3,3 +3,15 @@
 # Copyright permission under MIT License
 # All rights reserved by FayasNoushad
 # License -> https://github.com/FayasNoushad/Play-Store-Bot/blob/main/LICENSE
+
+from pyrogram import Client, filters
+
+
+Bot = Client(
+    "Play-Store-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+Bot.run()
