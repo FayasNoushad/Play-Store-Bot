13a924223e3d3cd821a95625855433d7ba588509
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-18 19:04:47 +0530

    Added private all filter

--- main.py
+++ main.py
@@ -14,4 +14,22 @@ Bot = Client(
     api_hash = os.environ["API_HASH"]
 )
 
+
+@Bot.on_message(filters.private & filters.all)
+async def filter_all(bot, update):
+    text = "Search play store apps using below buttons.\n\nMade by @FayasNoushad"
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton(text="Search here", switch_inline_query_current_chat="")],
+            [InlineKeyboardButton(text="Search in another chat", switch_inline_query="")]
+        ]
+    )
+    await update.reply_text(
+        text=text,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True,
+        quote=True
+    )
+
+
 Bot.run()
